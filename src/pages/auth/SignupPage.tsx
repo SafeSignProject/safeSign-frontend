5ca27f3b5cecf77d794e236714b3559f270831b5
정찬원 <jcw0522@gachon.ac.kr>
2026-04-13 21:47:46 +0900

    feat: 회원가입 페이지에도 헤더 적용

--- src/pages/auth/SignupPage.tsx
+++ src/pages/auth/SignupPage.tsx
@@ -1,3 +1,9 @@
+import AuthHeader from '@/components/header/AuthHeader';
+
 export const SignupPage = () => {
-  return <div></div>;
+  return (
+    <div className='flex flex-col items-center justify-center space-y-6'>
+      <AuthHeader title='회원가입' desc='계정을 만들어 시작하세요' />
+    </div>
+  );
 };
