152d13df7ed4a35a7bbe3af128cce62bb527df35
정찬원 <jcw0522@gachon.ac.kr>
2026-05-29 14:44:30 +0900

    feat: 어드민이면 어드민 페이지로만 이동

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,13 +1,25 @@
 import { Header } from '@/components/layouts';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
-import { Outlet, useLocation, Navigate } from 'react-router-dom';
+import useMyInfo from '@/hooks/useMyInfo';
+import { useEffect } from 'react';
+import { Outlet, useLocation, Navigate, useNavigate } from 'react-router-dom';
 
 const ProtectedLayout = () => {
   const location = useLocation();
+  const navigate = useNavigate();
 
   const { getAccessToken } = useLocalStorage();
 
   const token = getAccessToken();
+
+  const { data, isLoading } = useMyInfo();
+
+  // ADMIN 아니면 홈으로
+  useEffect(() => {
+    if (data?.role === 'ADMIN') {
+      navigate('/dashboard', { replace: true });
+    }
+  }, [data, isLoading, navigate]);
 
   // 홈("/")은 랜딩으로
   if (!token && location.pathname === '/') {
